ca4fc7649bc3235226bcbe3b9d7c2d7696fa8661
yeriinnn <angelrin7@naver.com>
2024-01-16 10:42:05 +0900

    [feat] outputstream에 대한 동기화 처리 여러 스레드 생성 및 작동 시 공유자원(outputstream)과 관련된 문제를 예방하기 위함

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,5 +1,3 @@
-package webserver;
-
 import java.io.*;
 import java.net.Socket;
 import java.nio.file.Files;
@@ -14,16 +12,18 @@ public class RequestHandler implements Runnable {
     private static final String WEB_ROOT = "src/main/resources/templates"; // 웹 리소스의 루트 디렉토리
 
     private Socket connection; // 클라이언트와의 연결을 담당하는 소켓
+    private OutputStream outputStream; // 외부에서 주입된 OutputStream
 
-    public RequestHandler(Socket connectionSocket) {
+    public RequestHandler(Socket connectionSocket, OutputStream outputStream) {
         this.connection = connectionSocket;
+        this.outputStream = outputStream;
     }
 
     public void run() {
         logger.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
                 connection.getPort());
 
-        try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
+        try (InputStream in = connection.getInputStream(); OutputStream out = outputStream) {
             BufferedReader reader = new BufferedReader(new InputStreamReader(in));
 
             // 요청 라인 읽기
@@ -52,32 +52,34 @@ public class RequestHandler implements Runnable {
 
         if (file.exists() && !file.isDirectory()) {
             byte[] body = Files.readAllBytes(filePath); // 파일 내용을 바이트 배열로 읽어옴
-            DataOutputStream dos = new DataOutputStream(out);
 
-            response200Header(dos, body.length); // 200 OK 응답 헤더 작성
-            responseBody(dos, body); // 응답 바디 작성
+            // 동기화 추가
+            synchronized (out) {
+                response200Header(out, body.length); // 200 OK 응답 헤더 작성
+                responseBody(out, body); // 응답 바디 작성
+            }
         } else {
             sendNotFoundError(out); // 파일이 존재하지 않을 경우 404 에러 응답
         }
     }
 
     // 200 OK 응답 헤더 작성
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
+    private void response200Header(OutputStream out, int lengthOfBodyContent) {
         try {
-            dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
-            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
-            dos.writeBytes("\r\n");
+            out.write("HTTP/1.1 200 OK \r\n".getBytes());
+            out.write("Content-Type: text/html;charset=utf-8\r\n".getBytes());
+            out.write(("Content-Length: " + lengthOfBodyContent + "\r\n").getBytes());
+            out.write("\r\n".getBytes());
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
     }
 
     // 응답 바디 작성
-    private void responseBody(DataOutputStream dos, byte[] body) {
+    private void responseBody(OutputStream out, byte[] body) {
         try {
-            dos.write(body, 0, body.length);
-            dos.flush();
+            out.write(body, 0, body.length);
+            out.flush();
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
@@ -85,12 +87,10 @@ public class RequestHandler implements Runnable {
 
     // 404 Not Found 에러 응답 작성
     private void sendNotFoundError(OutputStream out) throws IOException {
-        DataOutputStream dos = new DataOutputStream(out);
-
-        dos.writeBytes("HTTP/1.1 404 Not Found \r\n");
-        dos.writeBytes("Content-Type: text/plain;charset=utf-8\r\n");
-        dos.writeBytes("Content-Length: 0\r\n");
-        dos.writeBytes("\r\n");
-        dos.flush();
+        out.write("HTTP/1.1 404 Not Found \r\n".getBytes());
+        out.write("Content-Type: text/plain;charset=utf-8\r\n".getBytes());
+        out.write("Content-Length: 0\r\n".getBytes());
+        out.write("\r\n".getBytes());
+        out.flush();
     }
 }
