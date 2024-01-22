03526fcfdfd735f3a6e0209ce94b0852d433bca5
yeriinnn <angelrin7@naver.com>
2024-01-22 10:32:50 +0900

    [feat] controller 클래스 작성 완료 get으로 회원가입 기능 구현

---
+++ src/main/java/controller/Controller.java
@@ -0,0 +1,38 @@
+package controller;
+
+import annotation.GetMapping;
+import db.Database;
+import http.HttpResponse;
+import model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Map;
+
+public class Controller {
+    private static final Logger logger = LoggerFactory.getLogger(Controller.class);
+    private final String USER_ID = "userId";
+    private final String PASSWORD = "password";
+    private final String NAME = "name";
+    private final String EMAIL = "email";
+
+    // GET 회원가입
+    @GetMapping(value = "/user/create")
+    public String createUser(Map<String, String> query) {
+        logger.debug("Received GET request to create user.");
+
+        String userId = query.get(USER_ID);
+        String password = query.get(PASSWORD);
+        String name = query.get(NAME);
+        String email = query.get(EMAIL);
+
+        logger.debug("받은 유저 데이터 - userId: {}, password: {}, name: {}, email: {}", userId, password, name, email);
+
+        User user = new User(userId, password, name, email);
+
+        Database.addUser(user);
+        logger.debug("User created: {}", user);
+
+        return "redirect:/user/login.html";
+    }
+}
