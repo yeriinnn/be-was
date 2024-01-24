8444c4660c00ec57495e542abea90c52245bba45
yeriinnn <angelrin7@naver.com>
2024-01-24 12:07:34 +0900

    [feat] 로그추가 데이터베이스에 유저 생성되었는지 확인

--- src/main/java/db/Database.java
+++ src/main/java/db/Database.java
@@ -2,16 +2,22 @@ package db;
 
 import com.google.common.collect.Maps;
 
+import controller.Controller;
 import model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Collection;
 import java.util.Map;
 
 public class Database {
+    private static final Logger logger = LoggerFactory.getLogger(Database.class);
+
     private static Map<String, User> users = Maps.newHashMap();
 
     public static void addUser(User user) {
         users.put(user.getUserId(), user);
+        logger.debug("데이터베이스에 유저 추가됨: {}", user);
     }
 
     public static User findUserById(String userId) {
